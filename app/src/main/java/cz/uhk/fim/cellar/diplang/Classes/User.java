6a7e155258fafdc6b15dc3ee2f887b95cff9993a
Štěpán Cellar <stepan.cellar@gmail.com>
2021-06-23 00:10:00 +0200

    Initial commit

---
+++ app/src/main/java/cz/uhk/fim/cellar/diplang/Classes/User.java
@@ -0,0 +1,14 @@
+package cz.uhk.fim.cellar.diplang.Classes;
+
+public class User {
+
+    public String name, email;
+
+    public User(){}
+
+    public User(String name, String email){
+        this.name=name;
+        this.email=email;
+    }
+}
+
---
+++ app/src/main/java/cz/uhk/fim/cellar/diplang/RegisterActivity.java
@@ -0,0 +1,133 @@
+package cz.uhk.fim.cellar.diplang;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import cz.uhk.fim.cellar.diplang.Classes.User;
+
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText editTextName, editTextEmail, editTextPassword;
+    TextView registrace, banner;
+    Button buttonRegister;
+    ProgressBar progressBar;
+
+    private FirebaseAuth mAuth;
+    private FirebaseDatabase db = FirebaseDatabase.getInstance();
+    private DatabaseReference root = db.getReference();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+        banner = (TextView) findViewById(R.id.banner);
+        banner.setOnClickListener(this);
+
+        buttonRegister = (Button) findViewById(R.id.register);
+        buttonRegister.setOnClickListener(this);
+
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        editTextName = (EditText) findViewById(R.id.editTextName);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.banner:
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+            case R.id.register:
+                buttonRegister();
+                break;
+        }
+    }
+
+    private void buttonRegister() {
+        final String email = editTextEmail.getText().toString().trim();
+        final String password = editTextPassword.getText().toString().trim();
+        final String name = editTextName.getText().toString().trim();
+
+        if (name.isEmpty()) {
+            editTextName.setError("Jméno nesmí být prázdné");
+            editTextName.requestFocus();
+            return;
+        }
+        if (email.isEmpty()) {
+            editTextEmail.setError("Email nesmí být prázdný");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            editTextEmail.setError("Prosím zadejte validní email");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if (password.isEmpty()) {
+            editTextPassword.setError("Heslo nesmí být prázdné");
+            editTextPassword.requestFocus();
+            return;
+        }
+        if (password.length() < 6) {
+            editTextPassword.setError("Heslo musí obsahovat alespoň 6 znaků");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            User user = new User(name, email);
+                            FirebaseDatabase.getInstance().getReference("Users")
+                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid().toString())
+                                    .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if(task.isSuccessful()) {
+                                        Toast.makeText(RegisterActivity.this, "Uživatel byl úspěšně zaregistrován", Toast.LENGTH_LONG).show();
+                                    } else {
+                                        Toast.makeText(RegisterActivity.this, "Došlo k chybě, zkuste to prosím znovu", Toast.LENGTH_LONG).show();
+                                    }
+                                    progressBar.setVisibility(View.GONE);
+                                }
+                            });
+
+                        } else {
+                            Toast.makeText(RegisterActivity.this, "Došlo k chybě, zkuste to prosím znovu", Toast.LENGTH_LONG).show();
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+                });
+
+
+    }
+}
