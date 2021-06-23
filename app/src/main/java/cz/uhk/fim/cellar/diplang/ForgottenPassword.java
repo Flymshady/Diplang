3d18268b63f17e33a653a02f618c9e47dea9d356
Štěpán Cellar <stepan.cellar@gmail.com>
2021-06-23 21:19:07 +0200

    Profil uživatele & reset hesla

---
+++ app/src/main/java/cz/uhk/fim/cellar/diplang/ForgottenPassword.java
@@ -0,0 +1,68 @@
+package cz.uhk.fim.cellar.diplang;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthActionCodeException;
+
+public class ForgottenPassword extends AppCompatActivity {
+
+    private EditText editEmailAddress;
+    private Button buttonResetPassword;
+    private ProgressBar progressBar;
+
+    private FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgotten_password);
+
+        editEmailAddress = (EditText) findViewById(R.id.editEmailAddress);
+        buttonResetPassword = (Button) findViewById(R.id.buttonResetPassword);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        auth = FirebaseAuth.getInstance();
+        buttonResetPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                resetPassword();
+            }
+        });
+    }
+
+    private void resetPassword() {
+        String email = editEmailAddress.getText().toString().trim();
+
+        if(email.isEmpty()){
+            editEmailAddress.setError("Zadejte email");
+            editEmailAddress.requestFocus();
+            return;
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        auth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()){
+                Toast.makeText(ForgottenPassword.this, "Zkontrolujte Vaši emailovou schránku pro změnu hesla.", Toast.LENGTH_LONG).show();
+                }else{
+                Toast.makeText(ForgottenPassword.this, "Došlo k chybě, zkuste to prosím znovu.", Toast.LENGTH_LONG).show();
+                 }
+                progressBar.setVisibility(View.GONE);
+        }
+        });
+    }
+}
--- app/src/main/java/cz/uhk/fim/cellar/diplang/MainActivity.java
+++ app/src/main/java/cz/uhk/fim/cellar/diplang/MainActivity.java
@@ -10,7 +10,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -20,9 +19,8 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private TextView register, forgotten;
     private EditText email, password;
-    private Button login;
+    private Button login, register, forgotten;
     private FirebaseAuth mAuth;
     private ProgressBar progressBar;
 
@@ -34,13 +32,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         mAuth = FirebaseAuth.getInstance();
 
-        register = (TextView) findViewById(R.id.register);
+        register = (Button) findViewById(R.id.register);
         register.setOnClickListener(this);
+
+        forgotten = (Button) findViewById(R.id.forgotten);
+        forgotten.setOnClickListener(this);
 
         login = (Button) findViewById(R.id.login);
         login.setOnClickListener(this);
 
-        email = (EditText) findViewById(R.id.email);
+        email = (EditText) findViewById(R.id.emailTitle);
         password = (EditText) findViewById(R.id.password);
 
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
@@ -54,6 +55,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             case R.id.login:
                 userLogin();
+                break;
+            case R.id.forgotten:
+                startActivity(new Intent(this, ForgottenPassword.class));
                 break;
 
         }
@@ -92,8 +96,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                if(task.isSuccessful()) {
+                    //emial verifikace
+                   /*
+                   FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                   if(user.isEmailVerified()){
+                       //redirect to user profile
+                       startActivity(new Intent(MainActivity.this, ProfileActivity.class));
+                   }else {
+                       user.sendEmailVerification();
+                       Toast.makeText(MainActivity.this, "Nejprve potvrdtě registraci na Vašem emailu", Toast.LENGTH_LONG).show();
+                   }
+
+                    */
                    //redirect to user profile
                    startActivity(new Intent(MainActivity.this, ProfileActivity.class));
+
                }else{
                    Toast.makeText(MainActivity.this, "Došlo k chybě, zkuste to prosím znovu", Toast.LENGTH_LONG).show();
                    progressBar.setVisibility(View.GONE);
